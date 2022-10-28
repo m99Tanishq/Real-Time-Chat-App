d6df74b4cf48bb33554e1de360e29e2da4b4339a
m99Tanishq <21bcs164@ietdavv.edu.in>
2022-10-28 13:10:37 +0530

    Real-Time-Chat-App

---
+++ public/client.js
@@ -0,0 +1,48 @@
+const socket = io()
+
+const username = prompt('Please enter your username: ');
+let textarea = document.querySelector('textarea');
+let messageArea = document.querySelector('.message__area')
+
+
+textarea.addEventListener('keyup', (e) => {
+    if(e.key === 'Enter'){
+        sendMessage(e.target.value)
+    }
+})
+
+function sendMessage(message) {
+    let msg = {
+        user: username,
+        message: message.trim()
+    }
+    appendMessage(msg, 'outgoing')
+    textarea.value = '';
+    scrollToBottom();
+
+    socket.emit('message', msg)
+}
+
+function appendMessage(msg, type) {
+    let mainDiv = document.createElement('div');
+    let classname = type;
+    mainDiv.classList.add(classname, 'message');
+
+    let markup = `
+    <h4>${msg.user}</h4>
+    <p>${msg.message}</p>
+    `
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
+}
+
+
+socket.on('message', (msg) => {
+    appendMessage(msg, 'incoming')
+    scrollToBottom();
+})
+
+
+function scrollToBottom() {
+    messageArea.scrollTop = messageArea.scrollHeight
+}
